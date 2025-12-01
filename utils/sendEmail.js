60b31326ec70dbdffaac5ccd7f2fd0b895b23d82
NhatWang <24147110@student.hcmus.edu.vn>
2025-12-01 19:30:58 +0700

    Sua loi email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,29 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"Auth System" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    await transporter.sendMail({
+      from: `"XTN 2026" <no-reply@xtn.com>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("EMAIL SENT TO:", to);
+
+  } catch (err) {
+    console.error("EMAIL SEND ERROR:", err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
