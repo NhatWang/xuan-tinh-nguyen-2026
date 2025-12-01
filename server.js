7161f9cb9dce30bdb1961cf247981efdbc92b113
NhatWang <24147110@student.hcmus.edu.vn>
2025-12-01 19:04:05 +0700

    Fix tiep

--- server.js
+++ server.js
@@ -52,6 +52,5 @@ app.get("/", (req, res) => {
 // 7. START SERVER
 // ===============================
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
