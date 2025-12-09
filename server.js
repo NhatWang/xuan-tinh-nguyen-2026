114485837f8e7af90538d9fd9fea5d3eb32fc6ab
NhatWang <24147110@student.hcmus.edu.vn>
2025-12-09 17:01:49 +0700

    fix loi

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
+
+  // ⭐ THÊM NGAY DƯỚI ĐÂY
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Private-Network", "true");
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
