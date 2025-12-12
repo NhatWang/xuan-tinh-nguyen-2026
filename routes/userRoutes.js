14856b13ea5e5d306691c0dc5193f294a51aa695
NhatWang <24147110@student.hcmus.edu.vn>
2025-12-12 10:31:45 +0700

    fix crash

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
 const feedbackCtrl = require("../controllers/feedbackController");
 
 // Gửi feedback
 router.post("/feedback", auth, feedbackCtrl.submitFeedback);
-
-const feedbackCtrl = require("../controllers/feedbackController");
-
-router.get("/feedback/all", auth, admin, feedbackCtrl.getAllFeedback);
 
 
 module.exports = router;
